96e5e61c6736f0dbfddc51b0941ebe9c74521640
SiddhantShukla02 <77145400+SiddhantShukla02@users.noreply.github.com>
2025-11-28 16:06:55 +0530

    .

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: "https://test-healthconnect.netlify.app/",
+  origin: "https://test-healthconnect.netlify.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
